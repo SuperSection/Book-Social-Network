96081541a84cbee8302735a586235042b67225f4
SuperSection <soumosarkar297@gmail.com>
2024-07-21 21:06:41 +0530

    implement borrow book action & display message

--- book-network-ui/src/app/books/pages/book-list/book-list.component.ts
+++ book-network-ui/src/app/books/pages/book-list/book-list.component.ts
@@ -1,14 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { BookService } from '../../../services/services';
-import { PageResponseBookResponse } from '../../../services/models';
-import { NgFor } from '@angular/common';
-import { BookCardComponent } from "../../components/book-card/book-card.component";
+import {
+  BookResponse,
+  PageResponseBookResponse,
+} from '../../../services/models';
+import { NgFor, NgIf } from '@angular/common';
+import { BookCardComponent } from '../../components/book-card/book-card.component';
 
 @Component({
   selector: 'app-book-list',
   standalone: true,
-  imports: [NgFor, BookCardComponent],
+  imports: [NgFor, NgIf, BookCardComponent],
   templateUrl: './book-list.component.html',
   styleUrl: './book-list.component.scss',
 })
@@ -18,6 +21,8 @@ export class BookListComponent implements OnInit {
   bookResponse: PageResponseBookResponse = {};
   page: number = 0;
   size: number = 4;
+  message: string = '';
+  level: string = 'success';
 
   ngOnInit(): void {
     this.findAllBooks();
@@ -57,12 +62,30 @@ export class BookListComponent implements OnInit {
   }
 
   goToLastPage() {
-    this.page = this.bookResponse.totalPages as number - 1;
+    this.page = (this.bookResponse.totalPages as number) - 1;
     this.findAllBooks();
   }
-  
+
   get isLastPage(): boolean {
-    return this.page == (this.bookResponse.totalPages as number - 1);
+    return this.page == (this.bookResponse.totalPages as number) - 1;
+  }
+
+  borrowBook(book: BookResponse) {
+    this.message = '';
+    this.bookService
+      .borrowBook({
+        'book-id': book.id as number,
+      })
+      .subscribe({
+        next: () => {
+          this.level = 'success';
+          this.message = 'Book successfully added to your list.';
+        },
+        error: (error) => {
+          this.level = 'error';
+          this.message = error.error.error;
+        },
+      });
   }
 
 }
